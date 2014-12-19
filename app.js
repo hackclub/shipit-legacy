f5734b7a4ed2f25a05ef0f15d33bc7002c71080f
Zach Latta <zach@zachlatta.com>
2014-12-19 14:58:35 -0800

    properly resolve branch name conflicts

--- app.js
+++ app.js
@@ -66,13 +66,33 @@ function toSnakeCase(s) {
   return s.toLowerCase().split(' ').join('_');
 }
 
+// Generates a name for the branch for the project.
+//
+// If the spinal case version of the project is already taken, it then appends
+// incrementing numbers to it until it figures out a branch name that isn't
+// already taken.
+//
+// If the current branches are ['gh-pages', 'yelp-for-yelp-reviews',
+// 'yelp-for-yelp-reviews-1'] and the // project name is 'Yelp for Yelp
+// Reviews', then the generated branch name will be 'yelp-for-yelp-reviews-2'.
+function branchName(projectName, branches) {
+  var baseBranch = toSpinalCase(projectName);
+  var branch = baseBranch;
+
+  for (var i = 1; branches.indexOf(branch) > -1; i++) {
+    branch = baseBranch + '-' + i;
+  }
+
+  return branch;
+}
+
 app.get('/begin', function (req, res) {
   var github = new Github({
     token: req.session.passport.user.accessToken,
     auth: 'oauth'
   });
   var params = {
-    club: 'super_sample_club',
+    club: 'Super Sample Club',
     name: 'Yelp for Yelp Reviews',
     description: "It's like Yelp, but for Yelp Reviews",
     url: 'http://example.com',
@@ -83,7 +103,7 @@ app.get('/begin', function (req, res) {
   var repo = github.getRepo('hackedu', 'shipped');
   var forkedRepo =
     github.getRepo(req.session.passport.user.username, 'shipped');
-  var prBranch = toSpinalCase(params.name);
+  var prBranch;
 
   Q.nfcall(repo.fork)
   .then(function () { // poll for creation of new repo
@@ -92,7 +112,7 @@ app.get('/begin', function (req, res) {
     var timer = setTimeout(function () {
       forkedRepo.contents('gh-pages', '', function (err, contents) {
         if (err) {
-          console.log('error', err);
+          console.log('polling and got error', err);
           return
         }
 
@@ -104,7 +124,11 @@ app.get('/begin', function (req, res) {
     return deferred.promise;
   })
   .then(function () {
+    return Q.nfcall(forkedRepo.listBranches);
+  })
+  .then(function (branches) { // create branch for pull request
     var deferred = Q.defer();
+    prBranch = branchName(params.name, branches);
     forkedRepo.branch('gh-pages', prBranch, function (err) {
       if (err) {
         deferred.reject(err);
@@ -114,11 +138,11 @@ app.get('/begin', function (req, res) {
     });
     return deferred.promise;
   })
-  .then(function () {
-    var path = '_data/projects/'+toSnakeCase(params.club)+'/'+
-      toSnakeCase(params.name)+'.yml';
+  .then(function () { // create the project's .yml file
+    var path = '_data/projects/' + toSnakeCase(params.club) + '/' + 
+      toSnakeCase(params.name) + '.yml';
     var contents = ejs.render(projectTemplate, params);
-    var commitMsg = 'projects: add '+params.name;
+    var commitMsg = 'projects: add ' + params.name;
     return Q.nfcall(forkedRepo.write, prBranch, path, contents, commitMsg);
   })
   .catch(function (err) {
