86529f254257b319a308233910272b213c2ecb2b
Zach Latta <zach@zachlatta.com>
2014-12-18 18:20:52 -0800

    somewhat properly implement github login and start pr creation

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var config = require('./config.js');
 var express = require('express');
+var Github = require('github-api');
+var GithubStrategy = require('passport-github').Strategy;
 var http = require('http').Server(app);
 var passport = require('passport');
-var GithubStrategy = require('passport-github').Strategy;
+var Q = require('q');
+var session = require('express-session');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -21,22 +24,85 @@ passport.use(new GithubStrategy({
   callbackURL: config.github.callbackURL
 }, function (accessToken, refreshToken, profile, done) {
   process.nextTick(function () {
+    profile.accessToken = accessToken;
     return done(null, profile);
   });
 }));
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(session({
+  secret: config.session.secret,
+  resave: false,
+  saveUninitialized: true
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/auth/github', passport.authenticate('github'), function (req, res) {
+app.get('/auth/github', passport.authenticate('github', { scope: 'public_repo' }), function (req, res) {
 });
 
 app.get('/auth/github/callback', passport.authenticate('github'),
         function (req, res) {
-          res.redirect('/account');
+          res.redirect('/begin');
         });
+
+function toSpinalCase(s) {
+  return s.toLowerCase().split(' ').join('-');
+}
+
+app.get('/begin', function (req, res) {
+  var github = new Github({
+    token: req.session.passport.user.accessToken,
+    auth: 'oauth'
+  });
+  var params = {
+    club: 'super_sample_club',
+    name: 'Yelp for Yelp Reviews',
+    description: "It's like Yelp, but for Yelp Reviews",
+    url: 'http://example.com',
+    github_url: 'https://github.com/zachlatta/shipped',
+    authors: ['Zaphod Beeblebrox', 'Arthur Dent', 'Trillian Astra']
+  };
+
+  var repo = github.getRepo('hackedu', 'shipped');
+  var forkedRepo = github.getRepo(req.session.passport.user.username, 'shipped');
+
+  Q.nfcall(repo.fork)
+  .then(function () { // poll for creation of new repo
+    var deferred = Q.defer();
+
+    var timer = setTimeout(function () {
+      forkedRepo.contents('gh-pages', '', function (err, contents) {
+        if (err) {
+          console.log('error', err);
+          return
+        }
+
+        clearInterval(timer);
+        deferred.resolve(contents);
+      }, true);
+    }, 500);
+
+    return deferred.promise;
+  })
+  .then(function () {
+    var deferred = Q.defer();
+    forkedRepo.branch('gh-pages', toSpinalCase(params.name), function (err) {
+      if (err) {
+        deferred.reject(err);
+      } else {
+        deferred.resolve();
+      }
+    });
+    return deferred.promise;
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+  res.send('Done!');
+});
 
 app.get('/logout', function (req, res) {
   req.logout();
--- config.js
+++ config.js
@@ -3,6 +3,10 @@ dotenv.load();
 
 var config = {};
 
+config.session = {
+  secret: process.env.SESSION_SECRET
+}
+
 config.github = {
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
