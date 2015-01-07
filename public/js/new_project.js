903b0513c163a6c531abc7c990a83d563ecd77bd
Zach Latta <zach@zachlatta.com>
2015-01-06 16:27:46 -0800

    new project: make author fields removable. closes #7.

--- public/js/new_project.js
+++ public/js/new_project.js
@@ -9,21 +9,47 @@ var authorPlaceholders = [
   'Slartibartfast'
 ]
 
+function getRandomElementIn(arr) {
+  return arr[Math.floor(Math.random()*arr.length)];
+}
+
 addAuthorBtn.onclick = function () {
-  var br = document.createElement('br');
+  var authorDiv = document.createElement('div');
+  var inputWrapper = document.createElement('span');
   var input = document.createElement('input');
+  var removeWrapper = document.createElement('span');
+  var removeBtn = document.createElement('a');
+
+  authorDiv.setAttribute('class', 'author');
+
+  inputWrapper.setAttribute('class', 'inputWrapper');
 
   input.setAttribute('id', 'authors['+authorCount+']');
   input.setAttribute('type', 'text');
   input.setAttribute('name', 'authors['+authorCount+']');
   input.setAttribute('placeholder', getRandomElementIn(authorPlaceholders));
 
-  authors.appendChild(br);
-  authors.appendChild(input);
+  removeWrapper.setAttribute('class', 'removeWrapper');
+
+  removeBtn.setAttribute('class', 'remove-btn')
+  removeBtn.setAttribute('onclick', 'removeAuthorBtn(this)');
+  removeBtn.setAttribute('href', '#');
+  removeBtn.innerHTML = '[-]';
+
+  inputWrapper.appendChild(input);
+  removeWrapper.appendChild(removeBtn);
+
+  authorDiv.appendChild(inputWrapper);
+  authorDiv.appendChild(removeWrapper);
+
+  authors.appendChild(authorDiv);
 
   authorCount++;
 };
 
-function getRandomElementIn(arr) {
-  return arr[Math.floor(Math.random()*arr.length)];
+function removeAuthorBtn(btn) {
+  // Remove the parent's parent of the btn
+  var authorDivParent = btn.parentNode.parentNode.parentNode;
+  var authorDiv = btn.parentNode.parentNode;
+  return authorDivParent.removeChild(authorDiv);
 }
